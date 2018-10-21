5a4c3b34f78f3409800bc648588f544b873da1d3
grokify <johncwang@gmail.com>
2018-10-22 01:23:55 +0800

    add createenv option

--- apps/rchooks/main.go
+++ apps/rchooks/main.go
@@ -10,19 +10,19 @@ import (
 
 	"github.com/grokify/gotilla/config"
 	"github.com/grokify/gotilla/fmt/fmtutil"
-	iom "github.com/grokify/gotilla/io/ioutilmore"
 	"github.com/jessevdk/go-flags"
 
 	"github.com/grokify/rchooks"
 )
 
 type Options struct {
-	EnvFile  string `short:"e" long:"env" description:"Env filepath"`
-	WhichEnv []bool `short:"w" long:"which" description:"Which .env path"`
-	List     []bool `short:"l" long:"list" description:"List subscriptions"`
-	Create   string `short:"c" long:"create" description:"Create subscription"`
-	Delete   string `short:"d" long:"delete" description:"Delete subscription"`
-	Recreate string `short:"r" long:"recreate" description:"Recreate subscription"`
+	EnvFile   string `short:"e" long:"env" description:"Env filepath"`
+	WhichEnv  []bool `short:"w" long:"which" description:"Which .env path"`
+	List      []bool `short:"l" long:"list" description:"List subscriptions"`
+	Create    string `short:"c" long:"create" description:"Create subscription"`
+	CreateEnv []bool `long:"createenv" description:"Create subscription from environment"`
+	Delete    string `short:"d" long:"delete" description:"Delete subscription"`
+	Recreate  string `short:"r" long:"recreate" description:"Recreate subscription"`
 }
 
 func isUrl(s string) bool { return regexp.MustCompile(`^https?://`).MatchString(strings.ToLower(s)) }
@@ -37,32 +37,16 @@ func handleResponse(info interface{}, err error) {
 // This code takes a bot token and creates a permanent webhook.
 func main() {
 	opts := Options{}
-
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	envFilesToCheck := []string{opts.EnvFile, os.Getenv("ENV_PATH"), "./.env"}
-
-	for _, envFileTry := range envFilesToCheck {
-		envFileTry = strings.TrimSpace(envFileTry)
-		if len(envFileTry) > 0 {
-			if exists, err := iom.IsFileWithSizeGtZero(envFileTry); err != nil {
-				log.Fatal(err)
-			} else if !exists {
-				log.Fatal(fmt.Sprintf(".env file [%v] does not exist or is empty.", opts.EnvFile))
-			} else {
-				opts.EnvFile = envFileTry
-				break
-			}
-		}
-	}
-
-	if len(opts.EnvFile) > 0 {
-		if err := config.LoadDotEnvSkipEmpty(opts.EnvFile); err != nil {
-			log.Fatal(err)
-		}
+	err = config.LoadEnvPathsPrioritized(opts.EnvFile, os.Getenv("ENV_PATH"))
+	if err != nil {
+		fmt.Printf("E [%v] ENV [%v]\n", opts.EnvFile, os.Getenv("ENV_PATH"))
+		fmt.Println("H")
+		log.Fatal(err)
 	}
 
 	ctx := context.Background()
@@ -85,6 +69,10 @@ func main() {
 
 	if len(opts.List) > 0 {
 		handleResponse(rchooksUtil.GetSubscriptions(ctx))
+	}
+
+	if len(opts.CreateEnv) > 0 {
+		handleResponse(rchooksUtil.CreateSubscription(ctx, req))
 	}
 
 	if len(opts.Create) > 0 {
