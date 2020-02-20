790f5919e66dd512f1c7189c08a1a2b8ef9fbb56
John Wang <johncwang@gmail.com>
2020-02-20 11:48:24 -0800

    update test server

--- apps/test-server/main.go
+++ apps/test-server/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	validationToken := "Validation-Token"
 	if len(r.Header.Get(validationToken)) > 0 {
 		w.Header().Set(validationToken, r.Header.Get(validationToken))
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Warnf("E_CANNOT_READ_WEBHOOK_BODY [%v]", err.Error())
+	} else {
+		log.Infof("INCOMING_WEBHOOK_BODY [%v]", string(body))
 	}
 }
 
