cb7406ab704b0de293a740d843cdabaa0e22bf06
John Wang <johncwang@gmail.com>
2020-07-16 13:04:40 -0700

    feature: add NewRcHooksConfigCreds(), RcHooksConfig.Inflate()

--- config.go
+++ config.go
@@ -2,9 +2,13 @@ package rchooks
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"os"
+	"strings"
 
 	om "github.com/grokify/oauth2more"
+	"github.com/grokify/oauth2more/ringcentral"
 	"github.com/pkg/errors"
 
 	rc "github.com/grokify/go-ringcentral/office/v1/client"
@@ -18,6 +22,16 @@ type RcHooksConfig struct {
 	WebhookDefinition     rc.CreateSubscriptionRequest
 }
 
+func NewRcHooksConfigCreds(creds ringcentral.Credentials, hookDefJson string) RcHooksConfig {
+	cfg := RcHooksConfig{
+		ServerUrl:             creds.Application.ServerURL,
+		WebhookDefinitionJson: hookDefJson}
+	if creds.Token != nil {
+		cfg.Token = strings.TrimSpace(creds.Token.AccessToken)
+	}
+	return cfg
+}
+
 func NewRcHooksConfigEnv(envVarTokenOrJson, envVarServerUrl, envVarHookDef string) RcHooksConfig {
 	return RcHooksConfig{
 		Token:                 os.Getenv(envVarTokenOrJson),
@@ -25,19 +39,49 @@ func NewRcHooksConfigEnv(envVarTokenOrJson, envVarServerUrl, envVarHookDef strin
 		WebhookDefinitionJson: os.Getenv(envVarHookDef)}
 }
 
-func (appCfg *RcHooksConfig) InitilizeRcHooks(ctx context.Context) (RcHooks, error) {
+func (rchConfig *RcHooksConfig) Inflate() error {
+	rchConfig.WebhookDefinitionJson = strings.TrimSpace(rchConfig.WebhookDefinitionJson)
+	if len(rchConfig.WebhookDefinitionJson) <= 0 {
+		return fmt.Errorf("E_NO_WEBHOOK_DEFINITION")
+	}
+	req, err := ParseCreateSubscriptionRequest([]byte(rchConfig.WebhookDefinitionJson))
+	if err != nil {
+		return errors.Wrap(err, "Parse subscription definition")
+	}
+	rchConfig.WebhookDefinition = req
+	return nil
+}
+
+func (rchConfig *RcHooksConfig) Client() (*http.Client, error) {
+	return om.NewClientBearerTokenSimpleOrJson(context.Background(), []byte(rchConfig.Token))
+}
+
+func (rchConfig *RcHooksConfig) ClientUtil() (ringcentral.ClientUtil, error) {
+	cu := ringcentral.ClientUtil{
+		ServerURL: rchConfig.ServerUrl}
+	client, err := rchConfig.Client()
+	if err != nil {
+		return cu, err
+	}
+	cu.Client = client
+	return cu, err
+}
+
+func (rchConfig *RcHooksConfig) InitilizeRcHooks(ctx context.Context) (RcHooks, error) {
 	rchooksUtil := RcHooks{}
 
-	if req, err := ParseCreateSubscriptionRequest([]byte(appCfg.WebhookDefinitionJson)); err != nil {
-		return rchooksUtil, errors.Wrap(err, "Parse subscription definition")
-	} else {
-		appCfg.WebhookDefinition = req
+	err := rchConfig.Inflate()
+
+	// mt.Printf("RCHOOKS_INIT: [%v]\n", rchConfig.Token)
+
+	httpClient, err := rchConfig.Client()
+	if err != nil {
+		return rchooksUtil, errors.Wrap(err, "New client token")
 	}
 
-	if httpClient, err := om.NewClientBearerTokenSimpleOrJson(ctx, []byte(appCfg.Token)); err != nil {
-		return rchooksUtil, errors.Wrap(err, "New client token")
-	} else if apiClient, err := ru.NewApiClientHttpClientBaseURL(
-		httpClient, appCfg.ServerUrl); err != nil {
+	apiClient, err := ru.NewApiClientHttpClientBaseURL(
+		httpClient, rchConfig.ServerUrl)
+	if err != nil {
 		return rchooksUtil, err
 	} else {
 		rchooksUtil.Client = apiClient
