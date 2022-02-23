92b409a54ee18187623256bad9fecd1173cfb3ca
John Wang <johncwang@gmail.com>
2022-02-23 02:22:57 -0800

    dep: go mod: remove direct dependency `github.com/sirupsen/logrus`

--- cmd/test-server/main.go
+++ cmd/test-server/main.go
@@ -3,23 +3,22 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	validationToken := "Validation-Token"
 	if len(r.Header.Get(validationToken)) > 0 {
 		w.Header().Set(validationToken, r.Header.Get(validationToken))
-		log.Infof("INCOMING_WEBHOOK_VALIDATION_TOKEN [%v]", r.Header.Get(validationToken))
+		log.Printf("INCOMING_WEBHOOK_VALIDATION_TOKEN [%v]", r.Header.Get(validationToken))
 		return
 	}
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Warnf("E_CANNOT_READ_WEBHOOK_BODY [%v]", err.Error())
+		log.Printf("E_CANNOT_READ_WEBHOOK_BODY [%v]", err.Error())
 	} else {
-		log.Infof("INCOMING_WEBHOOK_BODY [%v]", string(body))
+		log.Printf("INCOMING_WEBHOOK_BODY [%v]", string(body))
 	}
 }
 
--- go.mod
+++ go.mod
@@ -8,5 +8,4 @@ require (
 	github.com/grokify/goauth v0.12.3
 	github.com/grokify/mogo v0.36.1
 	github.com/jessevdk/go-flags v1.5.0
-	github.com/sirupsen/logrus v1.8.1
 )
