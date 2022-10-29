c691a32950a3032d57cf332ccd025957d7b04983
John Wang <johncwang@gmail.com>
2022-10-29 14:22:09 -0700

    streamline: `cmd/test-server`: simplify code

--- cmd/test-server/main.go
+++ cmd/test-server/main.go
@@ -5,16 +5,18 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/grokify/rchooks"
 )
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
-	if len(r.Header.Get(rchooks.HeaderValidationToken)) > 0 {
-		w.Header().Set(rchooks.HeaderValidationToken, r.Header.Get(rchooks.HeaderValidationToken))
-		log.Printf("INCOMING_WEBHOOK_VALIDATION_TOKEN [%v]", r.Header.Get(rchooks.HeaderValidationToken))
+	if token := strings.TrimSpace(r.Header.Get(rchooks.HeaderValidationToken)); len(token) > 0 {
+		w.Header().Set(rchooks.HeaderValidationToken, token)
+		log.Printf("INCOMING_WEBHOOK_VALIDATION_TOKEN [%v]", token)
 		return
 	}
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("E_CANNOT_READ_WEBHOOK_BODY [%v]", err.Error())
